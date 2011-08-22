1dea1dc6d1ef5255aaac9a6cef8f5e80b660d107
Fredrik Leijon <fredrik.leijon@gmail.com>
2011-08-22 21:42:43 +0200

    Inital commit, using roboguice as ioc

---
+++ Publisher/src/com/tretton37/example/publisher/PublisherApplication.java
@@ -0,0 +1,16 @@
+package com.tretton37.example.publisher;
+
+import android.app.Application;
+import com.google.inject.Module;
+import roboguice.application.RoboApplication;
+
+import java.util.List;
+
+/**
+ * User: Fredrik / 2011-08-22
+ */
+public class PublisherApplication extends RoboApplication {
+     protected void addApplicationModules(List<Module> modules) {
+        modules.add(new PublisherModule());
+    }
+}
