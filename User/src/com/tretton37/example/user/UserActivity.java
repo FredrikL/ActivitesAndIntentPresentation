30aff9f0b06e00fba3ff9b09458ad2d79bf21715
Fredrik Leijon <fredrik.leijon@gmail.com>
2011-09-19 21:18:09 +0200

    Additional button for correct handling of calling intents that might not exist

--- User/src/com/tretton37/example/user/UserActivity.java
+++ User/src/com/tretton37/example/user/UserActivity.java
@@ -2,6 +2,8 @@ package com.tretton37.example.user;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
@@ -11,6 +13,8 @@ import android.widget.Toast;
 import roboguice.activity.RoboActivity;
 import roboguice.inject.InjectView;
 
+import java.util.List;
+
 public class UserActivity extends RoboActivity {
     @InjectView(R.id.btnCallIntent)
     Button btnCallIntent;
@@ -18,6 +22,8 @@ public class UserActivity extends RoboActivity {
     Button btnCallCameraIntent;
     @InjectView(R.id.btnCallUnknownIntent)
     Button btnCallUnknownInten;
+    @InjectView(R.id.btnCallUnknownIntentWithoutCrash)
+    Button btnCallUnknownIntentWithoutCrash;
 
     final int DEMO = 0;
     final int LOLS = 1;
@@ -44,6 +50,15 @@ public class UserActivity extends RoboActivity {
             }
         });
 
+        btnCallUnknownIntentWithoutCrash.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                if(canCallIntent("com.tretton37.example.publisher.LOLS")) {
+                    Intent intent = new Intent("com.tretton37.example.publisher.LOLS");
+                    startActivityForResult(intent, LOLS);
+                }
+            }
+        });
+
         btnCallCameraIntent.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
@@ -52,14 +67,22 @@ public class UserActivity extends RoboActivity {
         });
     }
 
+    private boolean canCallIntent(String Intent) {
+        PackageManager packageManager = getPackageManager();
+        Intent intent = new Intent(Intent);
+        List<ResolveInfo> list = packageManager.queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
+        return list.size() > 0;
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         Log.i("User", String.valueOf(requestCode));
         Log.i("User", String.valueOf(resultCode));
 
-        if (resultCode != Activity.RESULT_OK)
+        if (resultCode != Activity.RESULT_OK) {
+            Log.i("User", "meh!");
             return;
-
+        }
         switch (requestCode) {
             case DEMO:
                 String value = (String) data.getExtras().get("value");
@@ -68,6 +91,7 @@ public class UserActivity extends RoboActivity {
                 break;
 
             case CAMERA:
+                // show images
                 break;
 
             default:
